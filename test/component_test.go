83e32f564221c15da1e6ab7b184d21d9b8d62994
Benjamin Smith <ben.smith.developer@gmail.com>
2025-08-28 12:41:06 -0400

    update attributes

--- test/component_test.go
+++ test/component_test.go
@@ -24,35 +24,7 @@ func (s *ComponentSuite) TestBasic() {
 	defer s.DestroyAtmosComponent(s.T(), component, stack, nil)
 
 	inputs := map[string]interface{}{
-		"name": serviceName,
-		"containers": map[string]interface{}{
-			"service": map[string]interface{}{
-				"name":  "nginx",
-				"image": "nginx:stable",
-				"port_mappings": []map[string]interface{}{
-					{"containerPort": 80, "hostPort": 80, "protocol": "tcp"},
-				},
-			},
-		},
-		"task": map[string]interface{}{
-			"launch_type":   "FARGATE",
-			"network_mode":  "awsvpc",
-			"desired_count": 1,
-		},
-		// No LB for simpler test surface
-		"use_lb": false,
-
-		// Simple CIDR-based ingress rule to exercise new feature
-		"custom_security_group_rules": []map[string]interface{}{
-			{
-				"type":        "ingress",
-				"protocol":    "tcp",
-				"from_port":   80,
-				"to_port":     80,
-				"cidr_blocks": []string{"0.0.0.0/0"},
-				"description": "Allow HTTP from anywhere",
-			},
-		},
+		"attributes": []string{serviceName},
 	}
 
 	componentInstance, _ := s.DeployAtmosComponent(s.T(), component, stack, &inputs)
